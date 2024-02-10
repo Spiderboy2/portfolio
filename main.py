8d61bafe9302af8f5adbb830b7db79a3718ae9ab
Spider_Kid <sonichimman@gmail.com>
2024-02-10 14:55:17 +0530

    added description, image of project icon, and source code link. And changed the profile

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 with col1:
-    st.image("images/photo.png")
+    st.image("images/ME.png",)
 
 with col2:
     st.title("Chimman Soni")
@@ -18,14 +18,21 @@ with col2:
 content2 = """Below you can find some of the apps i have built in python. feel free to contact me!"""
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=",")
 
 with col3:
     for i, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
 
 with col4:
     for i, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+
